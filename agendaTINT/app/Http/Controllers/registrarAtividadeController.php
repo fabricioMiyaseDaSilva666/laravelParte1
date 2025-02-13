d93aebdf2f3ad4d6a5563fe069215e876b235a30
FABRICIO MIYASE DA SILVA <fabricio.msilva16@senacsp.edu.br>
2025-02-13 15:51:28 -0300

    terceiro commit

--- agendaTINT/app/Http/Controllers/registrarAtividadeController.php
+++ agendaTINT/app/Http/Controllers/registrarAtividadeController.php
@@ -23,4 +23,25 @@ class registrarAtividadeController extends Controller
         $model->save();//Armazenar no Banco de Dados
         return redirect('/cadastrar');
     }//Fim do método de cadastro
+
+    public function consultar(){
+        $ids = agendaModelo::all();
+        return view('paginas.consultar', compact('ids'));
+    }//Fim do método
+
+    public function editar($id){
+        $dado = agendaModelo::findOrFail($id);
+        return view('paginas.editar', compact('dado'));
+    }//Fim do método
+
+    public function atualizar(Request $request, $id)
+    {
+        agendaModelo::where('id',$id)->update($request->all());
+        return redirect('/consultar');
+    }//Fim do método atualizar
+
+    public function excluir(Request $request, $id){
+        agendaModelo::where('id',$id)->delete($request->all());
+        return redirect('/consultar');
+    }//Fim do método
 }//Todas as operações do banco de dados
